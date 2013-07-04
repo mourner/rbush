56c5f59f242b43c74490d9a6c51d1c61c4afc655
Vladimir Agafonkin <agafonkin@gmail.com>
2013-07-04 21:14:23 +0300

    add a second bbox for perf tests

--- debug/perf.js
+++ debug/perf.js
@@ -4,7 +4,8 @@ var data = [],
     x, y;
 
 var N = 1000000,
-    bbox = [49,72,51,74];
+    bbox = [49,72,51,74],
+    bbox2 = [23.5, 43.5, 23.6, 43.6];
 
 for (var i = 0; i < N; i++) {
     x = Math.random() * 99;
@@ -18,37 +19,40 @@ console.time('load');
 var tree = rbush().load(data);
 console.timeEnd('load');
 
-console.time('search');
+console.time('search 2x2 of 100x100');
 var result = tree.search(bbox);
-console.timeEnd('search');
+console.timeEnd('search 2x2 of 100x100');
 
-// console.time('naive search');
-// var result = [];
+console.time('search 0.1x0.1 of 100x100');
+var result = tree.search(bbox2);
+console.timeEnd('search 0.1x0.1 of 100x100');
+
+
+console.time('naive search');
+var result = [];
+for (var i = 0; i < N; i++) {
+ if (tree._contains(bbox, data[i])) {
+     result.push(data[i]);
+ }
+}
+console.timeEnd('naive search');
+
+
+// var RTree = typeof require !== 'undefined' ? require('rtree') : RTree;
+
+// var tree2 = new RTree();
+
+// var data2 = [];
 // for (var i = 0; i < N; i++) {
-//  if (tree._contains(bbox, data[i])) {
-//      result.push(data[i]);
-//  }
+//     data2.push({x: data[i][0], y: data[i][1], w: 0, h: 0});
 // }
-// console.timeEnd('naive search');
 
+// console.time('load 2');
+// for (var i = 0; i < N; i++) {
+//     tree2.insert(data2[i]);
+// }
+// console.timeEnd('load 2');
 
-RTree
-
-var RTree = typeof require !== 'undefined' ? require('rtree') : RTree;
-
-var tree2 = new RTree();
-
-var data2 = [];
-for (var i = 0; i < N; i++) {
-    data2.push({x: data[i][0], y: data[i][1], w: 0, h: 0});
-}
-
-console.time('load 2');
-for (var i = 0; i < N; i++) {
-    tree2.insert(data2[i]);
-}
-console.timeEnd('load 2');
-
-console.time('search 2');
-var result = tree2.search({x: bbox[0], y: bbox[1], w: bbox[2] - bbox[0], h: bbox[3] - bbox[1]});
-console.timeEnd('search 2');
+// console.time('search 2');
+// var result = tree2.search({x: bbox[0], y: bbox[1], w: bbox[2] - bbox[0], h: bbox[3] - bbox[1]});
+// console.timeEnd('search 2');
