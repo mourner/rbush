b947e4fd71973a4da7d8b09382bc02a92c817423
Vladimir Agafonkin <agafonkin@gmail.com>
2013-07-08 15:54:13 +0300

    cleanup perf code

--- debug/perf.js
+++ debug/perf.js
@@ -39,31 +39,25 @@ for (i = 0; i < 100; i++) {
 }
 console.timeEnd('100 searches 0.01%');
 
-console.time('100 choose subtree 1%');
-for (i = 0; i < 100; i++) {
-    tree._chooseSubtree(randBox(10), tree.data);
-}
-console.timeEnd('100 choose subtree 1%');
-
 console.time('100 choose subtree 0.01%');
 for (i = 0; i < 100; i++) {
     tree._chooseSubtree(randBox(1), tree.data);
 }
 console.timeEnd('100 choose subtree 0.01%');
 
-var result, bbox;
+// var result, bbox;
 
-console.time('100 naive searches 1%');
-for (var j = 0; j < 100; j++) {
-    result = [];
-    bbox = randBox(10);
-    for (i = 0; i < N; i++) {
-        if (tree._intersects(bbox, data[i])) {
-            result.push(data[i]);
-        }
-    }
-}
-console.timeEnd('100 naive searches 1%');
+// console.time('100 naive searches 1%');
+// for (var j = 0; j < 100; j++) {
+//     result = [];
+//     bbox = randBox(10);
+//     for (i = 0; i < N; i++) {
+//         if (tree._intersects(bbox, data[i])) {
+//             result.push(data[i]);
+//         }
+//     }
+// }
+// console.timeEnd('100 naive searches 1%');
 
 // var RTree = typeof require !== 'undefined' ? require('rtree') : RTree;
 
