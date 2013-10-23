6cc398b90599a7ba8b69d837114097d55e408b58
Konstantin Käfer <github@kkaefer.com>
2013-10-23 15:35:17 +0200

    don't use window object
    The window object doesn't exist in all JavaScript contexts, e.g. Web workers.

--- rbush.js
+++ rbush.js
@@ -491,7 +491,7 @@ if (typeof define === 'function' && define.amd) {
 } else if (typeof module !== 'undefined') {
     module.exports = rbush;
 } else {
-    window.rbush = rbush;
+    self.rbush = rbush;
 }
 
 })();
