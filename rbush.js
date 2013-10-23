1f0ff6c58fcecbf5c05b38883448e4450138ea8d
Konstantin Käfer <github@kkaefer.com>
2013-10-23 17:33:27 +0200

    use this instead of self

--- rbush.js
+++ rbush.js
@@ -491,7 +491,7 @@ if (typeof define === 'function' && define.amd) {
 } else if (typeof module !== 'undefined') {
     module.exports = rbush;
 } else {
-    self.rbush = rbush;
+    this.rbush = rbush;
 }
 
 })();
