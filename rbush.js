f9b0418d8381822e9f44f7d58ca4f324380a0c3f
Tom Payne <twpayne@gmail.com>
2013-11-04 21:52:21 +0100

    add _contains function to check whether a bbox completely contains another

--- rbush.js
+++ rbush.js
@@ -444,6 +444,13 @@ rbush.prototype = {
         }
     },
 
+    _contains: function(a, b) {
+        return a[0] <= b[0] &&
+               a[1] <= b[1] &&
+               b[2] <= a[2] &&
+               b[3] <= a[3];
+    },
+
     _intersects: function (a, b) {
         return b[0] <= a[2] &&
                b[1] <= a[3] &&
