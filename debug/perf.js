d5ca6dea58e548046ec5f33d9ed5131cc74c38dd
Vladimir Agafonkin <agafonkin@gmail.com>
2014-04-11 11:05:28 +0300

    more honest perf test

--- debug/perf.js
+++ debug/perf.js
@@ -1,10 +1,10 @@
-var rbush = typeof require !== 'undefined' ? require('../rbush.js') : rbush;
-
-var data = [],
-    x, y;
 
 var N = 1000000,
     maxFill = 16;
+
+console.log('number: ' + N);
+console.log('maxFill: ' + maxFill);
+
 
 function randBox(size) {
     var x = Math.random() * (100 - size),
@@ -14,15 +14,20 @@ function randBox(size) {
         y + size * Math.random()];
 }
 
-for (var i = 0; i < N; i++) {
-    data[i] = randBox(1);
+function genData(N, size) {
+    var data = [];
+    for (var i = 0; i < N; i++) {
+        data.push(randBox(size));
+    }
+    return data;
 }
 
-console.log('number: ' + N);
 
+var data = genData(N, 1);
+
+var rbush = typeof require !== 'undefined' ? require('../rbush.js') : rbush;
 var tree = rbush(maxFill);
 
-console.log('maxFill: ' + tree._maxEntries);
 
 console.time('insert one by one');
 for (i = 0; i < N; i++) {
@@ -30,27 +35,38 @@ for (i = 0; i < N; i++) {
 }
 console.timeEnd('insert one by one');
 
+
 // console.time('bulk load');
 // tree.load(data);
 // console.timeEnd('bulk load');
 
+
+var bboxes100 = genData(1000, 100 * Math.sqrt(0.1));
+
 console.time('1000 searches 10%');
 for (i = 0; i < 1000; i++) {
-    tree.search(randBox(100 * Math.sqrt(0.1)));
+    tree.search(bboxes100[i]);
 }
 console.timeEnd('1000 searches 10%');
 
+
+var bboxes10 = genData(1000, 10);
+
 console.time('1000 searches 1%');
 for (i = 0; i < 1000; i++) {
-    tree.search(randBox(10));
+    tree.search(bboxes10[i]);
 }
 console.timeEnd('1000 searches 1%');
 
+
+var bboxes1 = genData(1000, 1);
+
 console.time('1000 searches 0.01%');
 for (i = 0; i < 1000; i++) {
-    tree.search(randBox(1));
+    tree.search(bboxes1[i]);
 }
 console.timeEnd('1000 searches 0.01%');
+
 
 console.time('remove 1000 one by one');
 for (i = 0; i < 1000; i++) {
@@ -58,80 +74,91 @@ for (i = 0; i < 1000; i++) {
 }
 console.timeEnd('remove 1000 one by one');
 
-var data3 = [];
-for (var i = 0; i < N; i++) {
-    data3[i] = randBox(1);
-}
+
+var data2 = genData(N, 1);
 
 console.time('bulk-insert 1M more');
-tree.load(data3);
+tree.load(data2);
 console.timeEnd('bulk-insert 1M more');
+
 
 console.time('1000 searches 1%');
 for (i = 0; i < 1000; i++) {
-    tree.search(randBox(10));
+    tree.search(bboxes10[i]);
 }
 console.timeEnd('1000 searches 1%');
 
+
 console.time('1000 searches 0.01%');
 for (i = 0; i < 1000; i++) {
-    tree.search(randBox(1));
+    tree.search(bboxes1[i]);
 }
 console.timeEnd('1000 searches 0.01%');
 
 
-// var result, bbox;
+console.time('100 naive searches 1%');
+var result;
+for (var j = 0; j < 100; j++) {
+    result = [];
+    for (i = 0; i < N; i++) {
+        if (tree._intersects(bboxes10[j], data[i])) {
+            result.push(data[i]);
+        }
+    }
+}
+console.timeEnd('100 naive searches 1%');
 
-// console.time('100 naive searches 1%');
-// for (var j = 0; j < 100; j++) {
-//     result = [];
-//     bbox = randBox(10);
-//     for (i = 0; i < N; i++) {
-//         if (tree._intersects(bbox, data[i])) {
-//             result.push(data[i]);
-//         }
-//     }
-// }
-// console.timeEnd('100 naive searches 1%');
+
 
 var RTree = typeof require !== 'undefined' ? require('rtree') : RTree;
 
 var tree2 = new RTree(maxFill);
 
-var data2 = [];
-for (var i = 0; i < N; i++) {
-    data2.push({x: data[i][0], y: data[i][1], w: data[i][2] - data[i][0], h: data[i][3] - data[i][1]});
+function convertData(data) {
+    var result = [];
+    for (var i = 0; i < data.length; i++) {
+        result.push({x: data[i][0], y: data[i][1], w: data[i][2] - data[i][0], h: data[i][3] - data[i][1]});
+    }
+    return result;
 }
+
+var datab = convertData(data);
 
 console.time('old RTree load one by one');
 for (var i = 0; i < N; i++) {
-    tree2.insert(data2[i], i);
+    tree2.insert(datab[i], i);
 }
 console.timeEnd('old RTree load one by one');
 
+
+var bboxes100b = convertData(bboxes100);
+
 console.time('1000 searches 10% 2');
 for (i = 0; i < 1000; i++) {
-    bbox = randBox(100 * Math.sqrt(0.1));
-    tree2.search({x: bbox[0], y: bbox[1], w: bbox[2] - bbox[0], h: bbox[3] - bbox[1]});
+    tree2.search(bboxes100b[i]);
 }
 console.timeEnd('1000 searches 10% 2');
 
+
+var bboxes10b = convertData(bboxes10);
+
 console.time('1000 searches 1% 2');
 for (i = 0; i < 1000; i++) {
-    bbox = randBox(10);
-    tree2.search({x: bbox[0], y: bbox[1], w: bbox[2] - bbox[0], h: bbox[3] - bbox[1]});
+    tree2.search(bboxes10b[i]);
 }
 console.timeEnd('1000 searches 1% 2');
 
+
+var bboxes1b = convertData(bboxes1);
+
 console.time('1000 searches 0.01% 2');
 for (i = 0; i < 1000; i++) {
-    bbox = randBox(1);
-    tree2.search({x: bbox[0], y: bbox[1], w: bbox[2] - bbox[0], h: bbox[3] - bbox[1]});
+    tree2.search(bboxes1b[i]);
 }
 console.timeEnd('1000 searches 0.01% 2');
 
 console.time('old RTree remove 1000 one by one');
 for (var i = 0; i < 1000; i++) {
-    tree2.remove(data2[i], i);
+    tree2.remove(datab[i], i);
 }
 console.timeEnd('old RTree remove 1000 one by one');
