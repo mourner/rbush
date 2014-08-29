f2ba4bb9216b7e6abac90548cb28e9417fa8d9f0
Vladimir Agafonkin <agafonkin@gmail.com>
2014-08-30 02:14:11 +0300

    fix tests

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var rbush = require('../rbush.js'),
     t = require('tape');
 
 function sortedEqual(t, a, b, compare) {
-    t.same(a.sort(compare), b.sort(compare));
+    t.same(a.slice().sort(compare), b.slice().sort(compare));
 }
 
 function someData(n) {
@@ -21,28 +21,28 @@ var data = [[0,0,0,0],[10,10,10,10],[20,20,20,20],[25,0,25,0],[35,10,35,10],[45,
     [20,95,20,95],[25,75,25,75],[35,85,35,85],[45,95,45,95],[50,50,50,50],[60,60,60,60],[70,70,70,70],[75,50,75,50],
     [85,60,85,60],[95,70,95,70],[50,75,50,75],[60,85,60,85],[70,95,70,95],[75,75,75,75],[85,85,85,85],[95,95,95,95]];
 
-var testTree = {'children':[{
-    'children':[
-        {'children':[[0,0,0,0],[10,10,10,10],[20,20,20,20],[25,0,25,0]],      'leaf':true,'bbox':[0,0,25,20],  'height': 1},
-        {'children':[[35,10,35,10],[45,20,45,20],[50,0,50,0],[60,10,60,10]],  'leaf':true,'bbox':[35,0,60,20], 'height': 1},
-        {'children':[[0,25,0,25],[10,35,10,35],[20,45,20,45],[25,25,25,25]],  'leaf':true,'bbox':[0,25,25,45], 'height': 1},
-        {'children':[[35,35,35,35],[45,45,45,45],[50,25,50,25],[60,35,60,35]],'leaf':true,'bbox':[35,25,60,45],'height': 1}
-    ], 'bbox':[0,0,60,45], 'height': 2
-}, {
-    'children': [
-        {'children':[[0,50,0,50],[10,60,10,60],[20,70,20,70],[25,50,25,50]],  'leaf':true,'bbox':[0,50,25,70], 'height': 1},
-        {'children':[[35,60,35,60],[45,70,45,70],[50,50,50,50],[60,60,60,60]],'leaf':true,'bbox':[35,50,60,70],'height': 1},
-        {'children':[[0,75,0,75],[10,85,10,85],[20,95,20,95],[25,75,25,75]],  'leaf':true,'bbox':[0,75,25,95], 'height': 1},
-        {'children':[[35,85,35,85],[45,95,45,95],[50,75,50,75],[60,85,60,85]],'leaf':true,'bbox':[35,75,60,95],'height': 1}
-    ], 'bbox':[0,50,60,95], 'height': 2
-}, {
-    'children': [
-        {'children':[[70,20,70,20],[70,45,70,45],[75,0,75,0],[75,25,75,25]],  'leaf':true,'bbox':[70,0,75,45], 'height': 1},
-        {'children':[[85,10,85,10],[85,35,85,35],[95,20,95,20],[95,45,95,45]],'leaf':true,'bbox':[85,10,95,45],'height': 1},
-        {'children':[[70,70,70,70],[70,95,70,95],[75,50,75,50],[75,75,75,75]],'leaf':true,'bbox':[70,50,75,95],'height': 1},
-        {'children':[[85,60,85,60],[85,85,85,85],[95,70,95,70],[95,95,95,95]],'leaf':true,'bbox':[85,60,95,95],'height': 1}
-    ], 'bbox':[70,0,95,95], 'height': 2
-}], 'bbox':[0,0,95,95], 'height': 3};
+var testTree = {"children":[{
+    "children":[
+        {"children":[[0,0,0,0],[10,10,10,10],[20,20,20,20],[25,0,25,0]],"height":1,"bbox":[0,0,25,20],"leaf":true},
+        {"children":[[0,25,0,25],[10,35,10,35],[25,25,25,25],[20,45,20,45]],"height":1,"bbox":[0,25,25,45],"leaf":true},
+        {"children":[[50,0,50,0],[35,10,35,10],[60,10,60,10],[45,20,45,20]],"height":1,"bbox":[35,0,60,20],"leaf":true},
+        {"children":[[50,25,50,25],[35,35,35,35],[60,35,60,35],[45,45,45,45]],"height":1,"bbox":[35,25,60,45],"leaf":true}
+    ],"height":2,"bbox":[0,0,60,45]
+},{
+    "children":[
+        {"children":[[0,50,0,50],[25,50,25,50],[10,60,10,60],[20,70,20,70]],"height":1,"bbox":[0,50,25,70],"leaf":true},
+        {"children":[[0,75,0,75],[25,75,25,75],[10,85,10,85],[20,95,20,95]],"height":1,"bbox":[0,75,25,95],"leaf":true},
+        {"children":[[35,60,35,60],[50,50,50,50],[60,60,60,60],[45,70,45,70]],"height":1,"bbox":[35,50,60,70],"leaf":true},
+        {"children":[[50,75,50,75],[60,85,60,85],[45,95,45,95],[35,85,35,85]],"height":1,"bbox":[35,75,60,95],"leaf":true}
+    ],"height":2,"bbox":[0,50,60,95]
+},{
+    "children":[
+        {"children":[[70,20,70,20],[75,0,75,0],[75,25,75,25],[70,45,70,45]],"height":1,"bbox":[70,0,75,45],"leaf":true},
+        {"children":[[75,50,75,50],[70,70,70,70],[75,75,75,75],[70,95,70,95]],"height":1,"bbox":[70,50,75,95],"leaf":true},
+        {"children":[[85,10,85,10],[95,20,95,20],[85,35,85,35],[95,45,95,45]],"height":1,"bbox":[85,10,95,45],"leaf":true},
+        {"children":[[85,60,85,60],[95,70,95,70],[85,85,85,85],[95,95,95,95]],"height":1,"bbox":[85,60,95,95],"leaf":true}
+    ],"height":2,"bbox":[70,0,95,95]
+}],"height":3,"bbox":[0,0,95,95]};
 
 
 t('constructor accepts a format argument to customize the data format', function (t) {
@@ -118,8 +118,12 @@ t('#toBBox, #compareMinX, #compareMinY can be overriden to allow custom data str
 
 t('#load bulk-loads the given data given max node entries and forms a proper search tree', function (t) {
 
+    console.log(JSON.stringify(data));
+
     var tree = rbush(4).load(data);
     t.same(tree.toJSON(), testTree);
+
+    console.log("HELLO !");
     t.end();
 });
 
@@ -209,7 +213,7 @@ t('#all returns all points in the tree', function(t) {
 
 t('#toJSON & #fromJSON exports and imports search tree in JSON format', function (t) {
 
-    var tree = rbush(4);
+    var tree = rbush(4).load(data);
     tree.fromJSON(testTree);
 
     var tree2 = rbush(4).load(data);
