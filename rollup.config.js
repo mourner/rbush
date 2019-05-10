674711334468326dbe5433c3e9c537a744c805a6
Vladimir Agafonkin <agafonkin@gmail.com>
2019-05-10 19:11:15 +0300

    transpile UMD bundles with buble

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,19 @@
 import {terser} from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
+import buble from 'rollup-plugin-buble';
 
 const output = (file, plugins) => ({
     input: 'index.js',
     output: {
         name: 'rbush',
         format: 'umd',
+        indent: false,
         file
     },
     plugins
 });
 
 export default [
-    output('rbush.js', [resolve()]),
-    output('rbush.min.js', [resolve(), terser()])
+    output('rbush.js', [resolve(), buble()]),
+    output('rbush.min.js', [resolve(), buble(), terser()])
 ];
