89efe31d02178830353eb6e59e33985bda2d3b90
Vladimir Agafonkin <agafonkin@gmail.com>
2020-03-26 22:52:36 +0200

    upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import {terser} from 'rollup-plugin-terser';
-import resolve from 'rollup-plugin-node-resolve';
-import buble from 'rollup-plugin-buble';
+import resolve from '@rollup/plugin-node-resolve';
+import buble from '@rollup/plugin-buble';
 
 const output = (file, plugins) => ({
     input: 'index.js',
